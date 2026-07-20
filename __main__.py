6fd3a3b7f3da9988e28f04ebe26dea2bc5abecea
Edwino Stein <edwino.stein@gmail.com>
2026-07-19 22:35:58 -0700

    Refactor application runtime to make it more flexible

--- __main__.py
+++ __main__.py
@@ -1,9 +1,10 @@
 import sys
 import platform
+import importlib
 
 if tuple(map(int, platform.python_version_tuple())) <= (3, 13):
     raise RuntimeError('Invalid minimum Python version. Expecting >= 3.13.')
 
 if __name__ == '__main__':
-    import app
-    sys.exit(app.main(sys.argv[1:]))
+    app = importlib.import_module('app')
+    sys.exit(app.main(app.parse_args(sys.argv[1:])))
--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,39 +1,74 @@
 import argparse
 import asyncio
-from pprint import pprint
+import logging
+import signal
+import typing
+import importlib
 import RPi.GPIO as GPIO
-from .state import drive
-from .service import drive_service
-from pySMART import SMARTCTL
 
+_SERVICES = []
 
-SMARTCTL.sudo = True
-GPIO.setmode(GPIO.BCM)
+async def _start(args: argparse.Namespace) -> int:
+    logger = logging.getLogger(__name__)
 
+    logger.info('Starting application loop...')
 
-async def log_task():
-    while True:
-        print('Drives:', end=' ')
-        pprint(list(drive.iter_drives_state()))
-        await asyncio.sleep(5)
+    logger.debug('Main task renamed to "__main__".')
+    typing.cast(asyncio.Task, asyncio.current_task()).set_name('__main__')
 
+    logger.debug('Registering the signal INTerrupt event handler to the event loop.')
+    asyncio.get_running_loop().add_signal_handler(signal.SIGINT, _on_sigint)
 
-async def _main(args: argparse.Namespace) -> int:
+    logger.info('Loading services...')
+    for s in map(importlib.import_module, _SERVICES):
+        logger.info('Setting up service "%s"...', s.__name__)
+        await s.setup()
 
-    await asyncio.gather(
-        drive_service.monitor_all_drives(30),
-        log_task()
-    )
+    logger.info('Starting services...')
+    async with asyncio.TaskGroup() as tg:
+        for s in map(importlib.import_module, _SERVICES):
+            logger.info('Staring up service "%s"...', s.__name__)
+            tg.create_task(s.start())
 
+        logger.info('Application running, CTRL+C to stop...')
+
+    logger.info('Application stopped, bye!')
     return 0
 
 
-def _parse_args(argv: list[str]) -> argparse.Namespace:
-    return None
+async def _stop():
+    logger = logging.getLogger(__name__)
+
+    logger.info("Stopping services...")
+    for s in map(importlib.import_module, _SERVICES):
+        logger.info('Stopping service "%s"...', s.__name__)
+        await s.stop()
+
+    logger.info("Awaiting for service tasks....")
+    for task in list(filter(lambda t: t.get_name() not in ['__main__', '__stop__'], asyncio.all_tasks())):
+        task.cancel()
+        await asyncio.sleep(0.1)
 
 
-def main(argv: list[str]) -> int:
+def _on_sigint():
+    logger = logging.getLogger(__name__)
+    logger.info("Signal INTerrupt captured! Stopping application...")
+    asyncio.create_task(_stop(), name='__stop__')
+
+
+def parse_args(argv: list[str]) -> argparse.Namespace:
+    return argparse.Namespace()
+
+
+def main(args: argparse.Namespace) -> int:
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)6s [%(name)-15s] %(message)s')
+    logger = logging.getLogger(__name__)
+
+    logger.debug('GPIO set to BCM mode.')
+    GPIO.setmode(GPIO.BCM)
+
     try:
-        print(asyncio.run(_main(_parse_args(argv))))
-    except KeyboardInterrupt:
-        drive_service.stop_polling()
+        return asyncio.run(_start(args))
+    except:
+        logging.exception('Something bad happened:')
+        return 1
